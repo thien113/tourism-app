ea6ab90494cf8214bb7448ec91e78084faa040d3
thien113 <n-tv@hotmail.de>
2023-03-03 13:02:19 +0100

    connection to database is testes, image domain next config

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,27 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Button from "@/components/Button";
+import { useState, useEffect } from "react";
+import Card from "@/components/Card";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [attractionList, setAttractionList] = useState([]);
+  let attractions;
+  useEffect(() => {
+    fetch("/api/attractions")
+      .then((res) => res.json())
+      .then((data) => (attractions = data))
+      .then(() => {
+        console.log("index.js", attractions);
+      })
+      .then(() => {
+        setAttractionList(attractions);
+      });
+  }, []);
+  console.log(attractionList);
+
   return (
     <>
       <Head>
@@ -19,6 +36,10 @@ export default function Home() {
       </Head>
 
       <main>
+        {attractionList.map((attraction) => {
+          return <Card key={attraction.id} attraction={attraction} />;
+        })}
+
         <Button text="+Place" url="/new-attraction" />
       </main>
     </>
